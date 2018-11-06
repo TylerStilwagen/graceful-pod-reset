dad3274c9cc553c328bd93e48d000c9a383daf6e
flex1999 <44481720+flex1999@users.noreply.github.com>
2018-11-05 23:22:02 -0800

    Update 3pod-graceful-search-and-delete.ps1

--- 3pod-graceful-search-and-delete.ps1
+++ 3pod-graceful-search-and-delete.ps1
@@ -1,14 +1,15 @@
-﻿# The $null is so that memory in the variable isn't cached.
+# The $null is so that memory in the variable isn't cached.
 # $ErrorActionPreference = "SilentlyContinue"
 $Pods = $null
 $Region = $null
 $Region = Read-Host -Prompt 'Input region name'
 $NS = Read-Host -Prompt 'Input namespace'
-$Search = Read-Host -Prompt 'Input pod to search for'
+kubectl get pods -n $NS --context $Region
+$Search = Read-Host -Prompt 'Input unique search string'
 $Interval = Read-Host -Prompt 'Input time to delay between deletes'
 $Pods = kubectl get pods -n $NS --context $Region -o=custom-columns=NAME:.metadata.name | findstr $Search
 $Pods = $Pods.Trim(" NAME ")
-write-host Delete these pods in 10 seconds and counting  $Pods
+write-host Deleting these pods in 10 seconds: $Pods
 start-sleep -seconds 10
 foreach ($Items in $Pods) {
 kubectl delete pod -n $NS --context $Region $Items
